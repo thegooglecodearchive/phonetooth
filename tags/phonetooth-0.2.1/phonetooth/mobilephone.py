133cb41c9a4462c8402add6abe522f3b3fae49be
dirk.vdb <dirk.vdb@b851be54-1338-0410-84f5-8356642dce13>
2008-03-19 22:34:43 +0000

    Release 0.2.1

---
+++ tags/phonetooth-0.2.1/phonetooth/mobilephone.py
@@ -0,0 +1,218 @@
+#    Copyright (C) 2008 Dirk Vanden Boer <dirk.vdb@gmail.com>
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+import socket
+import bluetooth
+import obexftp
+import bit7alphabet
+
+from phonetooth import contacts
+from gettext import gettext as _
+
+class MobilePhone:
+    def __init__(self, connection):
+        self.__connection = connection
+        
+    
+    def __del__(self):
+        self.disconnect()
+        
+
+    def connect(self):
+        self.__connection.connect()
+        self.__sendATCommand('ATE0')
+
+        #use UTF-8 where possible
+        characterSets = self.__getSupportedCharacterSets()
+        if 'UTF-8' in characterSets:
+            self.__setCharacterSet('UTF-8')
+
+    
+    def disconnect(self):
+        self.__connection.disconnect()
+
+    
+    def getManufacturer(self):
+        return self.__sendATCommand('AT+CGMI')
+
+
+    def getModel(self):
+        return self.__sendATCommand('AT+CGMM')
+
+
+    def getSerialNumber(self):
+        return self.__sendATCommand('AT+CGSN')
+
+
+    def getBatteryStatus(self):
+        response = self.__sendATCommand('AT+CBC')
+        return int(response.split(',')[1])
+
+
+    def sendSMS(self, message, recipient):
+        supportedModes = self.__getSupportedSMSModes()
+        if '1' in supportedModes:
+            self.__sendSMSTextMode(message, recipient)
+        elif '0' in supportedModes:
+            self.__sendSMSPDUMode(message, recipient)
+        else:
+            raise Exception, _('Sending SMS not supported by phone')
+ 
+ 
+    def __sendSMSTextMode(self, message, recipient):
+        self.__sendATCommand('ATZ')
+        self.__sendATCommand('AT+CMGF=1') # text mode
+        
+        # send message information and wait for prompt
+        messageCommand = 'AT+CMGS="' + recipient + '"\r'
+        self.__connection.send(messageCommand)
+        reply = self.__connection.recv(len(messageCommand) + 4, wait=True) # read message + '\r\n> '
+
+        if reply[-2:] == '> ':
+            self.__sendATCommand(message + chr(26), False) # message + CTRL+Z
+        else:
+            raise Exception, _('Failed to send message')
+
+ 
+    def __sendSMSPDUMode(self, message, recipient):
+        self.__sendATCommand('ATZ')
+        self.__sendATCommand('AT+CMGF=0') # PDU mode
+        
+        if recipient[0] == '+':
+            recipient = recipient[1:]
+            addressType = '91'
+        else:
+            addressType = '81'
+            
+        #PDU Message
+        #11         SMS-Submit
+        #00         Message reference (Set by phone)
+        #length     Message Length
+        #81 or 91   Address type 81(national) 91 (international)
+        #phone nr
+        #00         Protocol
+        #00         Data coding scheme (7bit default alphabet)
+        #length     User data length
+        #user data
+        
+        pduMsg = '000100' + self.__byteToString(len(recipient))
+        pduMsg += addressType + self.__phoneNrToOctet(recipient)
+        pduMsg += '0000' + self.__byteToString(len(message))
+        msg7Bit = bit7alphabet.convert7BitToOctet(message)
+        for byte in msg7Bit:
+            pduMsg += self.__byteToString(byte)
+        
+        # send message information and wait for prompt
+        messageCommand = 'AT+CMGS=' + str(len(pduMsg) / 2 - 1) + '\r'
+        self.__connection.send(messageCommand)
+        reply = self.__connection.recv(len(messageCommand) + 4, wait=True) # read message + '\r\n> '
+
+        if reply[-2:] == '> ':
+            self.__sendATCommand(pduMsg + chr(26), False) # message + CTRL+Z
+        else:
+            raise Exception, _('Failed to send message')
+            
+    def getContacts(self, location):
+        if location == 'SIM':
+            self.__sendATCommand('AT+CPBS="SM"')
+        elif location == 'PHONE':
+            self.__sendATCommand('AT+CPBS="ME"')
+        else:
+            raise Exception, 'Invalid contact location specified'
+            
+        reply = self.__sendATCommand('AT+CPBR=?')
+        memorySlots = self.__parseMemorySlots(reply)
+        reply = self.__sendATCommand('AT+CPBR=1,' + memorySlots)
+        rawContacts = reply.strip().split('\r\n')
+        
+        contactList = []
+        for contact in rawContacts:
+            fields = contact.split(',')
+            contactList.append(contacts.Contact(fields[3][1:-1], fields[1][1:-1]))
+        
+        return contactList
+        
+        
+    def sendFile(self, filename):
+        client = obexftp.client(obexftp.BLUETOOTH)
+        client.connect(self.__connection.address, 9)
+        client.put_file(filename)
+        client.disconnect()
+        client.delete
+        
+        
+    def __sendATCommand(self, atCommand, lineBreak = True):
+        command = atCommand
+        if lineBreak:
+            command += '\r'
+        self.__connection.send(command)
+         
+        reply = ''
+        while not (reply.endswith('OK\r\n') or reply.endswith('ERROR\r\n')):
+            reply += self.__connection.recv()
+            
+        if reply.endswith('OK\r\n'):
+            end = reply.rfind('\r\nOK\r\n')
+            return reply[2:end]
+        else:
+            raise Exception, _('AT Command not supported by phone: ') + atCommand[:-1]
+            
+            
+    def __parseMemorySlots(self, reply):
+        #+CPBR:(1-100),40,14,0
+        begin   = reply.find('-') + 1
+        end     = reply.rfind(')')
+        return reply[begin:end]
+
+
+    def __setCharacterSet(self, characterSet):
+        self.__sendATCommand('AT+CSCS="' + characterSet + '"')
+        
+    
+    def __getSupportedCharacterSets(self):
+        response = self.__sendATCommand('AT+CSCS=?')
+        return self.__parseCommaseperatedList(response)
+
+
+    def __getSupportedSMSModes(self):
+        response = self.__sendATCommand('AT+CMGF=?')
+        return self.__parseCommaseperatedList(response)
+
+    
+    def __parseCommaseperatedList(self, list):
+        start = list.find('(') + 1
+        end = list.rfind(')')
+        return list[start:end].split(',')
+
+
+    def __phoneNrToOctet(self, phoneNr):
+        result = ''
+        i = 0
+        while (i + 1) < len(phoneNr):
+            result += phoneNr[i + 1]
+            result += phoneNr[i]
+            i += 2
+            
+        if len(phoneNr) % 2 != 0:
+            result += 'F'
+            result += phoneNr[len(phoneNr) - 1]
+            
+        return result
+        
+        
+    def __byteToString(self, byte):
+        return '%2.2X' % byte
+
---
+++ tags/phonetooth-0.2.1/phonetooth/mobilephonetest.py
@@ -0,0 +1,181 @@
+import unittest
+import mobilephone
+
+class BluetoothConnectionStub:
+    def __init__(self):
+        self.replies = []
+        self.recieves = []
+        
+    def connect(self):
+        pass
+            
+    def disconnect(self):
+        pass
+
+    def send(self, data):
+        self.recieves.append(data)
+
+    def recv(self, size = 0, wait = False):
+        if size != 0 and size != len(self.replies[0]):
+            raise Exception('Expected recieve size ' + str(len(self.replies[0])) + ' does not match ' + str(size))
+        
+        response = self.replies[0]
+        del self.replies[0]
+        return response
+
+class MobilePhoneTest(unittest.TestCase):
+    mobilePhone = None
+    connection = None
+    
+    def setUp(self):
+        self.connection = BluetoothConnectionStub()
+        self.mobilePhone = mobilephone.MobilePhone(self.connection)
+        
+    def tearDown(self):
+        del self.connection
+        self.connection = None
+        del self.mobilePhone
+        self.mobilePhone = None
+        
+    def assertSent(self, data):
+        self.assertEqual(data, self.connection.recieves[0])
+        del self.connection.recieves[0]
+
+    def testConnect(self):
+        self.connection.replies.append('\r\nOK\r\n')
+        self.connection.replies.append('\r\nOK\r\n')
+        
+        self.mobilePhone.connect()
+        
+        self.assertSent('ATE0\r')
+        self.assertSent('AT+CSCS=?\r')
+        self.assertEqual(0, len(self.connection.recieves))
+        self.assertEqual(0, len(self.connection.replies))
+        
+    def testConnectUtf8Supported(self):
+        self.connection.replies.append('\r\nOK\r\n')
+        self.connection.replies.append('(GSM,UTF-8)\r\nOK\r\n')
+        self.connection.replies.append('\r\nOK\r\n')
+        
+        self.mobilePhone.connect()
+        
+        self.assertSent('ATE0\r')
+        self.assertSent('AT+CSCS=?\r')
+        self.assertSent('AT+CSCS="UTF-8"\r')
+        self.assertEqual(0, len(self.connection.recieves))
+        self.assertEqual(0, len(self.connection.replies))
+
+    def testGetManufacturer(self):
+        self.connection.replies.append('\r\nManufacturer\r\nOK\r\n')
+        self.assertEqual('Manufacturer', self.mobilePhone.getManufacturer())
+        self.assertSent('AT+CGMI\r')
+        self.assertEqual(0, len(self.connection.recieves))
+        
+    def testGetModel(self):
+        self.connection.replies.append('\r\nModel\r\nOK\r\n')
+        self.assertEqual('Model', self.mobilePhone.getModel())
+        self.assertSent('AT+CGMM\r')
+        self.assertEqual(0, len(self.connection.recieves))
+
+    def testGetSerialNumber(self):
+        self.connection.replies.append('\r\n000-123456-000\r\nOK\r\n')
+        self.assertEqual('000-123456-000', self.mobilePhone.getSerialNumber())
+        self.assertSent('AT+CGSN\r')
+        self.assertEqual(0, len(self.connection.recieves))
+
+    def testGetBatteryStatus(self):
+        self.connection.replies.append('\r\n0,80\r\nOK\r\n')
+        self.assertEqual(80, self.mobilePhone.getBatteryStatus())
+        self.assertSent('AT+CBC\r')
+        self.assertEqual(0, len(self.connection.recieves))
+        
+    def testSendSMSTextMode(self):
+        self.connection.replies.append('\r\n(0,1)\r\nOK\r\n') #phone supports text and pdu, so text will be chosen
+        self.connection.replies.append('\r\nOK\r\n')
+        self.connection.replies.append('\r\nOK\r\n')
+        self.connection.replies.append('AT+CMGS="+320497123456"\r\r\n> ')
+        self.connection.replies.append('\r\nOK\r\n')
+        
+        self.mobilePhone.sendSMS('Message\r\nNext line', '+320497123456')
+        
+        self.assertSent('AT+CMGF=?\r')
+        self.assertSent('ATZ\r')
+        self.assertSent('AT+CMGF=1\r')
+        self.assertSent('AT+CMGS="+320497123456"\r')
+        self.assertSent('Message\r\nNext line' + chr(26))
+        self.assertEqual(0, len(self.connection.recieves))
+        self.assertEqual(0, len(self.connection.replies))
+        
+    def testSendSMSPDUMode(self):
+        self.connection.replies.append('\r\n(0)\r\nOK\r\n') #phone supports only pdu, so pdu will be chosen
+        self.connection.replies.append('\r\nOK\r\n')
+        self.connection.replies.append('\r\nOK\r\n')
+        self.connection.replies.append('AT+CMGS=22\r\r\n> ')
+        self.connection.replies.append('\r\nOK\r\n')
+        
+        self.mobilePhone.sendSMS('hellohello', '46708251358')
+        
+        self.assertSent('AT+CMGF=?\r')
+        self.assertSent('ATZ\r')
+        self.assertSent('AT+CMGF=0\r')
+        self.assertSent('AT+CMGS=22\r')
+        self.assertSent('0001000B816407281553F800000AE8329BFD4697D9EC37' + chr(26))
+        self.assertEqual(0, len(self.connection.recieves))
+        self.assertEqual(0, len(self.connection.replies))
+        
+    def testSendSMSPDUModeInternational(self):
+        self.connection.replies.append('\r\n(0)\r\nOK\r\n') #phone supports only pdu, so pdu will be chosen
+        self.connection.replies.append('\r\nOK\r\n')
+        self.connection.replies.append('\r\nOK\r\n')
+        self.connection.replies.append('AT+CMGS=22\r\r\n> ')
+        self.connection.replies.append('\r\nOK\r\n')
+        
+        self.mobilePhone.sendSMS('hellohello', '+46708251358')
+        
+        self.assertSent('AT+CMGF=?\r')
+        self.assertSent('ATZ\r')
+        self.assertSent('AT+CMGF=0\r')
+        self.assertSent('AT+CMGS=22\r')
+        self.assertSent('0001000B916407281553F800000AE8329BFD4697D9EC37' + chr(26))
+        self.assertEqual(0, len(self.connection.recieves))
+        self.assertEqual(0, len(self.connection.replies))
+        
+    def testGetContactsFromSIM(self):
+        self.connection.replies.append('\r\nOK\r\n')
+        self.connection.replies.append('\r\n+CPBR:(1-100),40,14,0\r\nOK\r\n')
+        self.connection.replies.append('+CPBR: 1,"1234",129,"Name 1",0\r\n'
+                                       '+CPBR: 2,"2345",129,"Name 2",0\r\n'
+                                       '+CPBR: 3,"3456",129,"Name 3",0\r\nOK\r\n')
+        
+        contacts = self.mobilePhone.getContacts('SIM')
+        
+        self.assertSent('AT+CPBS="SM"\r')
+        self.assertSent('AT+CPBR=?\r')
+        self.assertSent('AT+CPBR=1,100\r')
+        
+        self.assertEqual("1234", contacts[0].phoneNumber)
+        self.assertEqual("Name 1", contacts[0].name)
+        self.assertEqual("2345", contacts[1].phoneNumber)
+        self.assertEqual("Name 2", contacts[1].name)
+        self.assertEqual("3456", contacts[2].phoneNumber)
+        self.assertEqual("Name 3", contacts[2].name)
+        
+    def testGetContactsFromPhone(self):
+        self.connection.replies.append('\r\nOK\r\n')
+        self.connection.replies.append('\r\n+CPBR:(1-100),40,14,0\r\nOK\r\n')
+        self.connection.replies.append('+CPBR: 1,"1234",129,"Name 1",0\r\n'
+                                       '+CPBR: 2,"2345",129,"Name 2",0\r\n'
+                                       '+CPBR: 3,"3456",129,"Name 3",0\r\nOK\r\n')
+        
+        contacts = self.mobilePhone.getContacts('PHONE')
+        
+        self.assertSent('AT+CPBS="ME"\r')
+        self.assertSent('AT+CPBR=?\r')
+        self.assertSent('AT+CPBR=1,100\r')
+        
+        self.assertEqual("1234", contacts[0].phoneNumber)
+        self.assertEqual("Name 1", contacts[0].name)
+        self.assertEqual("2345", contacts[1].phoneNumber)
+        self.assertEqual("Name 2", contacts[1].name)
+        self.assertEqual("3456", contacts[2].phoneNumber)
+        self.assertEqual("Name 3", contacts[2].name)
