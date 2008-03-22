a304da76cf8afa74e9bd0ba38fb5e13e12fb5c5e
dirk.vdb <dirk.vdb@b851be54-1338-0410-84f5-8356642dce13>
2008-03-22 15:37:53 +0000

    Preferences added, seperated from preferencedialog

---
+++ phonetooth/preferences.py
@@ -0,0 +1,81 @@
+#    Copyright (C) 2008 Dirk Vanden Boer <dirk.vdb@gmail.com>
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+import os
+import bluetoothdiscovery
+import ConfigParser
+
+from gettext import gettext as _
+
+class Preferences:
+    def __init__(self, preferenceFile = None):
+        self.__preferenceFile = preferenceFile
+        self.backend = 'phonetooth'
+        self.btDevice = None
+        
+
+    def save(self):
+        config = ConfigParser.ConfigParser()
+        config.add_section('preferences')
+        config.set('preferences', 'backend', self.backend)
+        
+        if self.btDevice != None:
+            config.set('preferences', 'address', self.btDevice.address)
+            config.set('preferences', 'port', str(self.btDevice.port))
+            config.set('preferences', 'deviceName', self.btDevice.deviceName)
+            config.set('preferences', 'serviceName', self.btDevice.serviceName)
+        
+        if self.__preferenceFile == None:
+            self.__preferenceFile = self.__getPreferenceLocation()
+        
+        f = open(self.__preferenceFile, 'w')
+        config.write(f)
+        f.close()
+        
+    
+    def load(self):
+        try:
+            if self.__preferenceFile == None:
+                self.__preferenceFile = self.__getPreferenceLocation()
+            
+            config = ConfigParser.ConfigParser()
+            openedFiles = config.read(self.__preferenceFile)
+            
+            if len(openedFiles) == 0:
+                raise Exception, _('No config file found')
+                
+            try:
+                self.btDevice = bluetoothdiscovery.BluetoothDevice(
+                config.get('preferences', 'address'),
+                int(config.get('preferences', 'port')),
+                config.get('preferences', 'deviceName'),
+                config.get('preferences', 'serviceName'))
+            except:
+                self.btDevice == None
+                
+            self.backend = config.get('preferences', 'backend')
+        except Exception, e:
+            print _('Load config failed (reverting to defaults): ') + str(e)
+            self.btDevice = None
+
+
+    def __getPreferenceLocation(self):
+        configDir = os.path.join(os.path.expanduser('~'), '.config/phonetooth')
+        
+        if not os.path.isdir(configDir):
+            os.makedirs(configDir)
+            
+        return os.path.join(configDir, 'config')
