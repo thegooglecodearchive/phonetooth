7ad22a19c6e692403088e3a388ae3aa296fe4881
dirk.vdb <dirk.vdb@b851be54-1338-0410-84f5-8356642dce13>
2008-03-25 20:12:42 +0000

    Sms tests added Updated Makefile

--- trunk/testsuite.py
+++ trunk/testsuite.py
@@ -6,13 +6,15 @@ from phonetooth.bluetoothconnectiontest import BluetoothConnectionTest
 from phonetooth.preferencestest import PreferencesTest
 from phonetooth.contactstest import ContactsTest
 from phonetooth.bit7alphabettest import Bit7AlphabetTest
+from phonetooth.smstest import SmsTest
 
 allTests = unittest.TestSuite([
     unittest.TestLoader().loadTestsFromTestCase(MobilePhoneTest),
     unittest.TestLoader().loadTestsFromTestCase(BluetoothConnectionTest),
     unittest.TestLoader().loadTestsFromTestCase(PreferencesTest),
     unittest.TestLoader().loadTestsFromTestCase(ContactsTest),
-    unittest.TestLoader().loadTestsFromTestCase(Bit7AlphabetTest)
+    unittest.TestLoader().loadTestsFromTestCase(Bit7AlphabetTest),
+    unittest.TestLoader().loadTestsFromTestCase(SmsTest)
 ])
 
 unittest.TextTestRunner(verbosity=2).run(allTests)
